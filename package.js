47e0ed8d0603dbdea1eb80db47a0393803a2a956
sirprize <hoegl@backdoor.ch>
2013-06-09 23:39:05 +0200

    Initial import (history rewrite after being moved away from github.com/xsokev/Dojo-Bootstrap)

---
+++ package.js
@@ -0,0 +1,29 @@
+var profile = (function () {
+
+    var miniExcludes = {
+            "dobolo/package": 1
+        },
+        amdExcludes = {},
+        isTestRe = /\/tests\//
+    ;
+
+    return {
+        resourceTags: {
+            test: function (filename, mid) {
+                return isTestRe.test(filename);
+            },
+
+            miniExclude: function (filename, mid) {
+                return isTestRe.test(filename) || mid in miniExcludes;
+            },
+
+            amd: function (filename, mid) {
+                return /\.js$/.test(filename) && !(mid in amdExcludes);
+            },
+
+            copyOnly: function (filename, mid) {
+                return false;
+            }
+        }
+    };
+})();
---
+++ tests/assets/config.js
@@ -0,0 +1,11 @@
+var dojoConfig = {
+    async: 1,
+    locale: 'en',
+    cacheBust: 1,
+    parseOnLoad: true,
+    packages: [
+        { name: "mijit", location: "../../sirprize/mijit" },
+        { name: "dojo-form-controls", location: "../../sirprize/dojo-form-controls" },
+        { name: "dobolo", location: "../../.." }
+    ]
+};
